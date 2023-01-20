7edb1b0789b62ca1553658e14966bb857c1bc775
billy5982 <kuu0404@naver.com>
2023-01-20 23:26:45 +0900

    ✨ feat : 검색 결과 컴포넌트 통합 및 출력조건 함수 제작

--- src/components/ContentBox.tsx
+++ src/components/ContentBox.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
 import './ContentBox.css';
-import Title from './ContentBoxCp/Title';
+import ProductContent from './ContentBoxCp/ProductBox';
+import { productClassName } from '../libs/contentBox';
 
 export default function ContentBox() {
+  const data = {
+    id: 1,
+    title: 'iPhone 9',
+    description: 'An apple mobile which is nothing like apple',
+    price: 549,
+    discountPercentage: 12.96,
+    rating: 4.69,
+    stock: 94,
+    brand: 'Apple',
+    category: 'smartphones',
+    thumbnail: 'https://i.dummyjson.com/data/products/1/thumbnail.jpg',
+    images: [
+      'https://i.dummyjson.com/data/products/1/1.jpg',
+      'https://i.dummyjson.com/data/products/1/2.jpg',
+      'https://i.dummyjson.com/data/products/1/3.jpg',
+      'https://i.dummyjson.com/data/products/1/4.jpg',
+      'https://i.dummyjson.com/data/products/1/thumbnail.jpg',
+    ],
+  };
+
   return (
     <div className="ContentContainer">
-      <Title />
+      <ProductContent data={productClassName} type={'TitleBox'} />
+      <ProductContent data={data} type={'ProductBox'} />
       <div>{'페이지네이션 버튼 위치'}</div>
     </div>
   );
