b6360d441c89c5e80b0a7a0ed46a6bbc227d3151
billy5982 <kuu0404@naver.com>
2023-01-20 18:16:55 +0900

    ✨ feat : 검색 인풋 스타일링

--- src/components/SearchBoxCp/SearchInput.tsx
+++ src/components/SearchBoxCp/SearchInput.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import './SearchInput.css';
 export default function SearchInput() {
-  return <div>SearchInput</div>;
+  return <input placeholder="검색어를 입력해주세요" />;
 }
