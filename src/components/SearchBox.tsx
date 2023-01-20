2e43e1ae8bd41b31fb6a9a2ff3581050338accd1
billy5982 <kuu0404@naver.com>
2023-01-20 20:02:19 +0900

    ✨ feat : 검색창 스타일링

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import './SearchBox.css';
+import SearchBtn from './SearchBoxCp/SearchBtn';
+import SearchInput from './SearchBoxCp/SearchInput';
+import SelectBox from './SearchBoxCp/SelectBox';
 
 export default function SearchBox() {
-  return <div className="SearchBox">SearchBox</div>;
+  return (
+    <div className="SearchBox">
+      <SelectBox />
+      <SearchInput />
+      <SearchBtn />
+    </div>
+  );
 }
---
+++ src/types/searchBox.d.ts
@@ -0,0 +1,7 @@
+export type SearchList = {
+  [index: string]: string;
+  all: string;
+  productName: string;
+  brand: string;
+  productContent: string;
+};
