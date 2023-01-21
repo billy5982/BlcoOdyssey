a652f5086e66a2a8d31aa6145c7c958337c323e3
billy5982 <kuu0404@naver.com>
2023-01-21 16:50:40 +0900

    ✨ feat : 검색결과 표시 완료

--- src/components/ContentBox.tsx
+++ src/components/ContentBox.tsx
@@ -2,40 +2,27 @@ import React from 'react';
 import './ContentBox.css';
 import ProductContent from './ContentBoxCp/ProductBox';
 import { productClassName } from '../libs/contentBox';
+import { useSelector } from 'react-redux';
+import { RootState } from '../store/store';
 
 export default function ContentBox() {
-  const data = {
-    id: 1,
-    title: 'iPhone 9',
-    description: 'An apple mobile which is nothing like apple',
-    price: 549,
-    discountPercentage: 12.96,
-    rating: 4.69,
-    stock: 94,
-    brand: 'Apple',
-    category: 'smartphones',
-    thumbnail: 'https://i.dummyjson.com/data/products/1/thumbnail.jpg',
-    images: [
-      'https://i.dummyjson.com/data/products/1/1.jpg',
-      'https://i.dummyjson.com/data/products/1/2.jpg',
-      'https://i.dummyjson.com/data/products/1/3.jpg',
-      'https://i.dummyjson.com/data/products/1/4.jpg',
-      'https://i.dummyjson.com/data/products/1/thumbnail.jpg',
-    ],
-  };
+  const { filterContent, dataLoading } = useSelector(
+    (state: RootState) => state.searchInfo
+  );
 
   return (
     <div className="content-wrapper">
       <div className="ContentContainer">
         <ProductContent data={productClassName} type={'TitleBox'} />
-        <ProductContent data={data} type={'ProductBox'} />
-        <ProductContent data={data} type={'ProductBox'} />
-        <ProductContent data={data} type={'ProductBox'} />
-        <ProductContent data={data} type={'ProductBox'} />
-        <ProductContent data={data} type={'ProductBox'} />
-        <ProductContent data={data} type={'ProductBox'} />
-        <ProductContent data={data} type={'ProductBox'} />
-        <ProductContent data={data} type={'ProductBox'} />
+        {dataLoading
+          ? filterContent.map((product) => (
+              <ProductContent
+                key={product.id}
+                data={product}
+                type={'ProductBox'}
+              />
+            ))
+          : null}
       </div>
       <div>{'페이지네이션 버튼 위치'}</div>
     </div>
