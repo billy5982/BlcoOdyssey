6705a05ba312c43b7c9e92a0d425fae3db8660c7
billy5982 <kuu0404@naver.com>
2023-01-21 10:35:09 +0900

    ✨ feat : 리덕스 init,action 세팅 완료

--- src/store/reducer/searchInfo.ts
+++ src/store/reducer/searchInfo.ts
@@ -1,5 +1,10 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { SearchSetting } from '../../types/reduxState';
+import {
+  SearchSetting,
+  ContentArr,
+  PageRow,
+  currentPage,
+} from '../../types/reduxState';
 import { SearchBar } from '../../types/reduxState';
 
 export const searchInfo = createSlice({
@@ -7,14 +12,23 @@ export const searchInfo = createSlice({
   initialState: {
     searchKind: 'all', // 검색하고자 하는 종류
     searchContent: '', // 검색하고자 하는 내용
-    showNum: 10, // 컨텐츠를 보고 싶은 갯수
-    content: null, // 불러온 컨텐츠
+    pageRow: '10', // 컨텐츠를 보고 싶은 갯수
+    content: [], // 불러온 컨텐츠
     curPage: 1, // 현재 페이지
   } as SearchSetting, // 필수로 타입 지정 안해도 되지만, 확실히 하기로 한다.
   // dispatch 함수
   reducers: {
     setSearchKind(state, action: PayloadAction<SearchBar>) {
       return { ...state, searchKind: action.payload.searchKind };
+    },
+    setPageRow(state, action: PayloadAction<PageRow>) {
+      return { ...state, pageRow: action.payload.pageRow };
+    },
+    setContent(state, action: PayloadAction<ContentArr>) {
+      return { ...state, content: action.payload.content };
+    },
+    setCurPage(state, action: PayloadAction<currentPage>) {
+      return { ...state, curPage: action.payload.curPage };
     },
   },
 });
--- src/types/reduxState.d.ts
+++ src/types/reduxState.d.ts
@@ -4,9 +4,18 @@ export interface SearchBar {
   searchKind: string; // 검색하고자 하는 종류
   searchContent: string | null; // 검색하고자 하는 내용
 }
+export interface ContentArr {
+  content: [] | Titles[];
+}
+export interface PageRow {
+  pageRow: string;
+}
+export interface currentPage {
+  curPage: number;
+}
 
 export interface SearchSetting extends SearchBar {
-  showNum: number; // 컨텐츠를 보고 싶은 갯수
-  content: null | Titles[]; // 불러온 컨텐츠
+  pageRow: string; // 컨텐츠를 보고 싶은 갯수
   curPage: number; // 현재 페이지
+  content: [] | Titles[]; // 불러온 컨텐츠
 }
